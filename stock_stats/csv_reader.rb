9f263442462c1bf8bb386bf43b09aad9994106b5
Surya DaVinci <suryadavinci@gmail.com>
2017-05-03 14:56:51 +0530

    csv reader working well

--- stock_stats/csv_reader.rb
+++ stock_stats/csv_reader.rb
@@ -1,16 +1,18 @@
 require 'csv'
-require_relative 'books_in_stock'
+require_relative 'BookInStock'
 class CsvReader
+  attr_reader :books_in_stock
+
   def initialize
     @books_in_stock = []
   end
 
   def read_in_csv_data(csv_file_name)
-    CSV.foreach(csv_file_name, header: true) do |row|
+    puts "test"
+    CSV.foreach(csv_file_name, headers: true) do |row|
       @books_in_stock << BookInStock.new(row["ISBN"], row["Price"])
+      #puts "wow"
     end
+    #CSV.foreach(csv_file_name, header: true) do |row|      @books_in_stock << BookInStock.new(row["ISBN"], row["Price"])    end
   end
 end
-
-CsvReader csv = CsvReader.new
-csv.read_in_csv_data(data.csv)
