747e5d95f4d00d647caa13c8b62132ea80b9136d
Surya DaVinci <suryadavinci@gmail.com>
2017-05-03 18:01:25 +0530

    updated

--- stock_stats/csv_reader.rb
+++ stock_stats/csv_reader.rb
@@ -1,7 +1,7 @@
 require 'csv'
 require_relative 'BookInStock'
 class CsvReader
-  attr_reader :books_in_stock
+  attr_accessor :books_in_stock
 
   def initialize
     @books_in_stock = []
@@ -10,9 +10,15 @@ class CsvReader
   def read_in_csv_data(csv_file_name)
     puts "test"
     CSV.foreach(csv_file_name, headers: true) do |row|
-      @books_in_stock << BookInStock.new(row["ISBN"], row["Price"])
+      @books_in_stock << BookInStock.new(row["ISBN"], Float(row["Price"]))
       #puts "wow"
     end
     #CSV.foreach(csv_file_name, header: true) do |row|      @books_in_stock << BookInStock.new(row["ISBN"], row["Price"])    end
+  end
+
+  def total_val_in_stock
+    sum = 0.0
+    @books_in_stock.each {|book| sum = sum + book.price }
+    sum
   end
 end
--- stock_stats/stock_stats.rb
+++ stock_stats/stock_stats.rb
@@ -1,14 +1,28 @@
 require_relative 'csv_reader'
 reader = CsvReader.new
 
-ARGV.each do |csv_file_name|
-STDERR.puts "Processing #{csv_file_name}"
-reader.read_in_csv_data(csv_file_name)
-end
-p reader.instance_variable_get(:@books_in_stock)
-p reader.books_in_stock
-puts reader.books_in_stock
+#ARGV.each do |csv_file_name|
+#STDERR.puts "Processing #{csv_file_name}"
+#reader.read_in_csv_data(csv_file_name)
+#end
 
+reader.read_in_csv_data(ARGV[0].to_s)
+
+p reader.books_in_stock
+
+puts "============"
+puts "using do end"
+puts "============"
 reader.books_in_stock.each do |val|
   p "isbn #{val.isbn} #{val.price}"
 end
+puts "========"
+puts
+puts "========"
+puts "using {}"
+puts "========"
+reader.books_in_stock.each { |val|  p "isbn #{val.isbn} #{val.price}"}
+
+
+
+p "Total Value #{reader.total_val_in_stock}"
